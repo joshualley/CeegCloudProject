e7d6d10d768492f27b649c4d719a4de7556a9644
joshualley <ly.silent.valley@gmail.com>
2021-05-27 13:32:40 +0800

    销售订单

--- PythonPlugIn/ERP/SaleOrder.py
+++ PythonPlugIn/ERP/SaleOrder.py
@@ -34,8 +34,8 @@ def DataChanged(e):
 		sql = "EXEC proc_czly_GetOrgDeptBySalemanId @SmId='{}'".format(FSalerId)
 		results = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
 		if results.Count > 0:
-			this.View.Model.SetValue("FSaleDeptId", str(results[0]["FBizDeptID"]))
-			this.View.Model.SetValue("FSaleDeptId", str(results[0]["FBizDeptID"]), -1)
+			this.Model.SetValue("FSaleDeptId", str(results[0]["FBizDeptID"]))
+			this.Model.SetValue("FSaleDeptId", str(results[0]["FBizDeptID"]), -1)
 
 def BeforeDoOperation(e):
 	_opKey = e.Operation.FormOperation.Operation
@@ -113,29 +113,41 @@ def SumAmtAndFilterRejectedRow():
 
 def AlterCustAndSalerByOrg():
 	"""替换客户，销售员"""
-	FSaleOrgId = 0 if this.View.Model.GetValue("FSaleOrgId") == None else str(this.View.Model.GetValue("FSaleOrgId")["Id"])
+	FSaleOrgId = 0 if this.Model.GetValue("FSaleOrgId") == None else str(this.Model.GetValue("FSaleOrgId")["Id"])
 	if FSaleOrgId == 0:
 		return
-	FSrcBillNo = "" if this.View.Model.GetValue("FSrcBillNo", 0) == None else str(this.View.Model.GetValue("FSrcBillNo", 0))
+	FSrcType = "" if this.Model.GetValue("FSrcType", 0) == None else str(this.Model.GetValue("FSrcType", 0))
+	FSrcBillNo = "" if this.Model.GetValue("FSrcBillNo", 0) == None else str(this.Model.GetValue("FSrcBillNo", 0))
 	FSalerId, FCustId = 0, 0
 	#查询是否为销售合同
-	sql = "SELECT FSaler,FCustName FROM ora_CRM_Contract WHERE FBILLNO='{}'".format(FSrcBillNo)
-	srcBill = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
-	if srcBill.Count > 0:
-		FSalerId = srcBill[0]["FSaler"]
-		FCustId = srcBill[0]["FCustName"]
+	if FSrcType == "ora_CRM_Contract":
+		sql = "SELECT FSaler,FCustName FROM ora_CRM_Contract WHERE FBILLNO='{}'".format(FSrcBillNo)
+		srcBill = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
+		if srcBill.Count > 0:
+			FSalerId = srcBill[0]["FSaler"]
+			FCustId = srcBill[0]["FCustName"]
 	#内部合同
-	sql = "SELECT FSaler,FCustName FROM ora_CRM_InnerContract WHERE FBILLNO='{}'".format(FSrcBillNo)
-	srcBill = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
-	if srcBill.Count > 0:
-		FSalerId = srcBill[0]["FSaler"]
-		FCustId = srcBill[0]["FCustName"]
+	elif FSrcType == "ora_CRM_InnerContract":
+		sql = "SELECT FSaler,FCustName FROM ora_CRM_InnerContract WHERE FBILLNO='{}'".format(FSrcBillNo)
+		srcBill = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
+		if srcBill.Count > 0:
+			FSalerId = srcBill[0]["FSaler"]
+			FCustId = srcBill[0]["FCustName"]
 	#维修合同
-	sql = "SELECT FSalerID,FCustID FROM ora_CRM_MtnCont WHERE FBILLNO='{}'".format(FSrcBillNo)
-	srcBill = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
-	if srcBill.Count > 0:
-		FSalerId = srcBill[0]["FSalerID"]
-		FCustId = srcBill[0]["FCustID"]
+	elif FSrcType == "ora_CRM_MantainContract":
+		sql = "SELECT FSalerID,FCustID FROM ora_CRM_MtnCont WHERE FBILLNO='{}'".format(FSrcBillNo)
+		srcBill = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
+		if srcBill.Count > 0:
+			FSalerId = srcBill[0]["FSalerID"]
+			FCustId = srcBill[0]["FCustID"]
+	elif FSrcType == "SAL_SaleOrder":
+		sql = "SELECT FSalerId,FCustId FROM T_SAL_ORDER WHERE FBILLNO='{}'".format(FSrcBillNo)
+		srcBill = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
+		if srcBill.Count > 0:
+			FSalerId = srcBill[0]["FSalerID"]
+			FCustId = srcBill[0]["FCustID"]
+	else:
+		return
 
 	if FSalerId == 0 or FCustId == 0:
 		this.Model.SetValue("FSalerId", FSalerId)
@@ -148,16 +160,16 @@ def AlterCustAndSalerByOrg():
 		and FNUMBER=(select FNUMBER from T_BD_CUSTOMER where FCUSTID='{}')""".format(FSaleOrgId, FCustId)
 	results = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
 	if results.Count > 0:
-		this.View.Model.SetValue("FCustId", str(results[0]["FCUSTID"]))
+		this.Model.SetValue("FCustId", str(results[0]["FCUSTID"]))
 	# 获取销售员内码
 	sql = """SELECT FID FROM V_BD_SALESMAN WHERE FBIZORGID='{}' AND 
 		FNUMBER=(SELECT FNUMBER FROM V_BD_SALESMAN WHERE FID='{}')""".format(FSaleOrgId, FSalerId)
 	results = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
 	if results.Count > 0:
-		this.View.Model.SetValue("FSalerId", str(results[0]["FID"]))
+		this.Model.SetValue("FSalerId", str(results[0]["FID"]))
 		# 获取办事处
 		sql = "EXEC proc_czly_GetOrgDeptBySalemanId @SmId='{}'".format(results[0]["FID"])
 		results = DBUtils.ExecuteDataSet(this.Context, sql).Tables[0].Rows
 		if results.Count > 0:
-			this.View.Model.SetValue("FSaleDeptId", str(results[0]["FBizDeptID"]))
+			this.Model.SetValue("FSaleDeptId", str(results[0]["FBizDeptID"]))
 
