1bbdcc3e30399d3105f2df39d7508009a508af17
onechance <zhouh08@hotmail.com>
2022-04-06 11:23:31 +0800

    报表加字段 消息提醒

--- PlugIns/PMT/CZ.CEEG.BosPmt.PmtSummary/CZ_CEEG_BosPmt_PmtSummary.cs
+++ PlugIns/PMT/CZ.CEEG.BosPmt.PmtSummary/CZ_CEEG_BosPmt_PmtSummary.cs
@@ -1,19 +1,18 @@
 ﻿using Kingdee.BOS.App.Data;
 using Kingdee.BOS.Core;
 using Kingdee.BOS.Core.Bill;
-using Kingdee.BOS.Core.Bill.PlugIn;
 using Kingdee.BOS.Core.DynamicForm;
 using Kingdee.BOS.Core.DynamicForm.PlugIn;
 using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
 using Kingdee.BOS.Core.Metadata;
+using Kingdee.BOS.Msg;
 using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.BOS.ServiceHelper.Messages;
 using Kingdee.BOS.Util;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CZ.CEEG.BosPmt.PmtSummary
 {
@@ -31,13 +30,13 @@ namespace CZ.CEEG.BosPmt.PmtSummary
             string sDt = currDt.Year.ToString() + "-" + currDt.Month.ToString() + "-01";
             string eDt = currDt.ToString();
 
-            //设置开始日期为订单最早日期
+            /*//设置开始日期为订单最早日期
             string sql = "SELECT TOP 1 FDate FROM T_SAL_ORDER ORDER BY FDate ASC";
             var obj = DBUtils.ExecuteDynamicObject(this.Context, sql);
             if (obj.Count > 0)
             {
                 sDt = obj[0]["FDate"].ToString();
-            }
+            }*/
 
             this.Model.SetValue("FSDate", sDt);
             this.View.UpdateView("FSDate");
@@ -56,6 +55,125 @@ namespace CZ.CEEG.BosPmt.PmtSummary
                     Act_QuerySummaryData();
                     break;
             }
+        }
+
+        public override void EntryBarItemClick(BarItemClickEventArgs e)
+        {
+            base.EntryBarItemClick(e);
+            string key = e.BarItemKey;
+            switch (key)
+            {            
+                case "ora_send_msg":
+                    SendMsg();
+                    break;
+            }
+        }
+
+        private void SendMsg()
+        {
+            int rowNum = this.View.Model.GetEntryRowCount("FEntity");
+
+            List<Dictionary<string, string>> sendInfos = new List<Dictionary<string, string>>();
+
+            for (int i = 0; i < rowNum; i++)
+            {
+                bool check = (bool)this.Model.GetValue("FSendCheck", i);
+                if (check)
+                {
+                    Dictionary<string, string> info = new Dictionary<string, string>
+                        {
+                            { "FSeller", this.Model.GetValue("FSeller", i)==null?"":this.Model.GetValue("FSeller", i).ToString() },
+                            { "FCusName", this.Model.GetValue("FCusName", i)==null?"":this.Model.GetValue("FCusName", i).ToString() },
+                            { "FOrderDate", this.Model.GetValue("FOrderDate", i)==null?"":this.Model.GetValue("FOrderDate", i).ToString() },
+                            { "FwillOverAmt", this.Model.GetValue("FwillOverAmt", i)==null?"":this.Model.GetValue("FwillOverAmt", i).ToString() },
+                            { "FOPENID", this.Model.GetValue("FOPENID", i)==null?"":this.Model.GetValue("FOPENID", i).ToString() },
+                            { "FLaterDelvGoodsDt", this.Model.GetValue("FLaterDelvGoodsDt", i)==null?"":this.Model.GetValue("FLaterDelvGoodsDt", i).ToString() },
+                            { "FDeadline", this.Model.GetValue("FDeadline", i)==null?"":this.Model.GetValue("FDeadline", i).ToString() },
+                            { "FOrderNo",this.Model.GetValue("FOrderNo", i) ==null?"":this.Model.GetValue("FOrderNo", i).ToString() }
+                        };
+                    sendInfos.Add(info);
+                }
+            }
+
+            if (sendInfos.Count==0)
+            {
+                this.View.ShowErrMessage("请选择!");
+            }
+            else {
+
+                String now = DateTime.Now.ToString("yyyy-MM-dd");
+                String appId = "PmtSummary";
+                String title = "title";
+                String PubAcctCode = "XT-4249be9f-5181-4a2a-a01f-7a9be93f1b3a";
+                String PubAcctKey = "4839223c64dff23b04bf4e3376b7a082";
+
+                List<Dictionary<string, string>> sendLogs = new List<Dictionary<string, string>>();
+
+                sendInfos.ForEach(info =>
+                {
+                    string text = "";
+                    //判断是否超期,选择不同模板
+                    DateTime deadLine = Convert.ToDateTime(info["FDeadline"]);
+                    if (DateTime.Compare(deadLine, DateTime.Now) < 0)
+                    {
+                        text = string.Format(@"亲爱的中电家人[{0}],您所在手的订单编号为[{1}],所属客户为[{2}],订单签订时间为[{3}],订单最后一次发货时间为[{4}],该订单存在[{5}]的逾期金额,逾期日期为[{6}],请及时跟踪并反馈货款情况.谢谢",
+                                            info["FSeller"], info["FOrderNo"], info["FCusName"], info["FOrderDate"],
+                                            info["FLaterDelvGoodsDt"], info["FwillOverAmt"], info["FDeadline"]);
+                    }
+                    else {
+                        text = string.Format(@"亲爱的中电家人[{0}],您所在手的订单编号为[{1}],所属客户为[{2}],订单签订时间为[{3}],订单最后一次发货时间为[{4}],该订单即将有[{5}]的逾期金额,逾期日期为[{6}],请及时跟踪并反馈货款情况.谢谢",
+                                            info["FSeller"], info["FOrderNo"], info["FCusName"], info["FOrderDate"], 
+                                            info["FLaterDelvGoodsDt"], info["FwillOverAmt"], info["FDeadline"]);
+                    }
+
+                    Kingdee.BOS.Msg.XunTongMessage message = new Kingdee.BOS.Msg.XunTongMessage
+                    {
+                        AppId = appId,
+                        Title = title,
+                        Date = DateTime.Now.ToString("yyyy-MM-dd"),
+                        Text = text,
+                        PubAcctCode = PubAcctCode,//公共号帐号，移动BOS
+                        PubAcctKey = PubAcctKey,//公共号密钥
+                        Users = new List<string> { info["FOPENID"] }
+                    };
+
+                    XTSendResult res = XunTongServiceHelper.SendMessage(this.Context, message);
+
+                    Dictionary<string, string> log = new Dictionary<string, string>
+                    {
+                        { "order", info["FOrderNo"] }
+                    };
+
+                    if (res.IsSuccess)
+                    {
+                        log.Add("res", "success");
+                    }
+                    else
+                    {
+                        log.Add("res", "error");
+                        log.Add("message", res.Msg);
+                    }
+
+                    sendLogs.Add(log);
+
+                });
+
+                string errorMessages = "";
+
+                sendLogs.Where(log => log["res"].Equals("error")).ToList().ForEach(log =>
+                {
+                    errorMessages += log["order"] + "发送失败:" + log["message"] + "\n\r";
+                });
+
+                if (!errorMessages.Equals(""))
+                {
+                    this.View.ShowErrMessage(errorMessages.Substring(1));
+                }
+                else
+                {
+                    this.View.ShowMessage("消息已发送");
+                }
+            }   
         }
 
         public override void EntityRowDoubleClick(EntityRowClickEventArgs e)
@@ -159,8 +277,7 @@ namespace CZ.CEEG.BosPmt.PmtSummary
             string FQOrderNo = this.Model.GetValue("FQOrderNo") == null ? "" : this.Model.GetValue("FQOrderNo").ToString().Trim();
 
 
-            string sql = string.Format(@"exec proc_czly_GetPmt @FormId='{0}', @SDt='{1}', @EDt='{2}', 
-@FQDeptId='{3}', @FQSalerId={4}, @FQCustId={5}, @FQFactoryId='{6}', @FQOrderNo='{7}'",
+            string sql = string.Format(@"exec proc_czly_GetPmt @FormId='{0}', @SDt='{1}', @EDt='{2}', @FQDeptId='{3}', @FQSalerId={4}, @FQCustId={5}, @FQFactoryId='{6}', @FQOrderNo='{7}'",
             formid, FSDate, FEDate, deptSql, FQSalerId, FQCustId, FQFactoryId, FQOrderNo);
             var objsDB = DBUtils.ExecuteDynamicObject(this.Context, sql);
             this.Model.DeleteEntryData("FEntity");
@@ -185,6 +302,12 @@ namespace CZ.CEEG.BosPmt.PmtSummary
 
                 }
                 )).ToList();
+            }
+
+            //过滤截止日期
+            string FDeadlineQuery = this.Model.GetValue("FDeadlineQuery") == null ? "" : this.Model.GetValue("FDeadlineQuery").ToString();
+            if (!FDeadlineQuery.Equals("")) {
+                objs = objs.Where(o => o["FDeadline"].ToString().Equals(FDeadlineQuery)).ToList();
             }
 
             this.Model.BatchCreateNewEntryRow("FEntity", objs.Count);
@@ -227,6 +350,12 @@ namespace CZ.CEEG.BosPmt.PmtSummary
                 this.Model.SetValue("FIsOldSysOrder", FIsOldSysOrder, i);
                 this.Model.SetValue("FDeliverNote", objs[i]["FDeliverNote"].ToString(), i);
                 this.Model.SetValue("FDelvPmt", objs[i]["FDelvPmt"].ToString(), i);
+                this.Model.SetValue("FDeadline", objs[i]["FDeadline"].ToString(), i);
+                this.Model.SetValue("FOPENID", objs[i]["FOPENID"].ToString(), i);
+                this.Model.SetValue("FSeller", objs[i]["FSeller"].ToString(), i);
+                this.Model.SetValue("FCusName", objs[i]["FCusName"].ToString(), i);
+                this.Model.SetValue("FOrderDate", objs[i]["FOrderDate"].ToString(), i);
+                this.Model.SetValue("FwillOverAmt", objs[i]["FwillOverAmt"].ToString(), i);
             }
             this.View.UpdateView("FEntity");
         }
--- PlugIns/PMT/CZ.CEEG.BosPmt.PmtSummary/TenderBond/TenderBondDyForm.cs
+++ PlugIns/PMT/CZ.CEEG.BosPmt.PmtSummary/TenderBond/TenderBondDyForm.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Kingdee.BOS.App.Data;
 using Kingdee.BOS.Core.Bill;
 using Kingdee.BOS.Core.DynamicForm;
 using Kingdee.BOS.Core.DynamicForm.PlugIn;
 using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Msg;
+using Kingdee.BOS.ServiceHelper.Messages;
 using Kingdee.BOS.Util;
 
 namespace CZ.CEEG.BosPmt.PmtSummary.TenderBond
@@ -41,6 +45,9 @@ namespace CZ.CEEG.BosPmt.PmtSummary.TenderBond
                 case "ORA_TBTRANS": // ora_tbTrans 转移保证金
                     Act_Trans();
                     break;
+                case "ORA_SEND_MSG2":
+                    SendMsg();
+                    break;
             }
         }
 
@@ -57,7 +64,7 @@ namespace CZ.CEEG.BosPmt.PmtSummary.TenderBond
                 isnull(bd.FInvoiceAmt, 0) FInvoiceAmt, isnull(bd.FBidSrvFee, 0) FBidSrvFee,
                 isnull(bd.FReturnAmt, 0) FReturnAmt,
                 case when isnull(tp.FCAPTION, '')='' and isnull(bd.FID, 0)<>0 then '中标'
-                     else isnull(tp.FCAPTION, '') end FTransNote
+                     else isnull(tp.FCAPTION, '') end FTransNote,u.fname,x.FOPENID,pf.FBACKDATE,pf.FTENDERTIME,pf.FCREATEDATE,cus2.FNAME as FCONTRACTPARTY 
             from ora_t_Cust100011 pf
             left join ora_PMT_Bidding bd on bd.FPubFundID=pf.FID and bd.FDocumentStatus in ('B', 'C')
             left join ora_PMT_MarginTrans mt on mt.FPubFundID=pf.FID and mt.FDocumentStatus in ('B', 'C')
@@ -68,7 +75,13 @@ namespace CZ.CEEG.BosPmt.PmtSummary.TenderBond
                 INNER JOIN T_META_FORMENUMITEM fei ON fel.FID=fei.FID
                 INNER JOIN T_META_FORMENUMITEM_L feil ON feil.FENUMID=fei.FENUMID
                 WHERE FNAME='PMT转移类型'
-            ) tp on tp.FVALUE=mt.FTransType
+            ) tp on tp.FVALUE=mt.FTransType 
+             join T_HR_EMPINFO emp on emp.fid = pf.fapplyid
+			 join V_bd_ContactObject obj on emp.FNUMBER = obj.FNUMBER
+			 join T_SEC_USER u on u.FLINKOBJECT=obj.FID 
+			 join T_SEC_XTUSERMAP x on u.FUSERID = x.FUSERID 
+             join T_BD_CUSTOMER cus on cus.FCUSTID = pf.FCONTRACTPARTY 
+			 join T_BD_CUSTOMER_L cus2 on cus2.FCUSTID = cus.FCUSTID
             where pf.FCostType in ('3', '10') and pf.FDocumentStatus='C'
             and pf.FCreateDate between '{0}' and '{1}'
             order by pf.FCreateDate desc", FBeginDt, FEndDt);
@@ -89,9 +102,112 @@ namespace CZ.CEEG.BosPmt.PmtSummary.TenderBond
                     this.Model.SetValue("FBidSrvFee", item["FBidSrvFee"], i);
                     this.Model.SetValue("FReturnAmt", item["FReturnAmt"], i);
                     this.Model.SetValue("FTransNote", item["FTransNote"], i);
+                    this.Model.SetValue("FSeller", item["fname"], i);
+                    this.Model.SetValue("FOpenid", item["FOPENID"], i);
+                    this.Model.SetValue("FBackDate", item["FBACKDATE"], i);
+                    this.Model.SetValue("FTENDERTIME", item["FTENDERTIME"], i);
+                    this.Model.SetValue("FCREATEDATE", item["FCREATEDATE"], i);
+                    this.Model.SetValue("FCONTRACTPARTY", item["FCONTRACTPARTY"], i);
                 }
             }
             this.View.UpdateView("FEntity");
+        }
+
+        private void SendMsg()
+        {
+            int rowNum = this.View.Model.GetEntryRowCount("FEntity");
+
+            List<Dictionary<string, string>> sendInfos = new List<Dictionary<string, string>>();
+
+            for (int i = 0; i < rowNum; i++)
+            {
+                bool check = (bool)this.Model.GetValue("FSendCheck", i);
+                if (check)
+                {
+                    Dictionary<string, string> info = new Dictionary<string, string>
+                        {
+                            { "FOPENID", this.Model.GetValue("FOpenid", i)==null?"":this.Model.GetValue("FOpenid", i).ToString() },
+                            { "FBackDate", this.Model.GetValue("FBackDate", i)==null?"":this.Model.GetValue("FBackDate", i).ToString() },
+                            { "FTENDERTIME", this.Model.GetValue("FTENDERTIME", i)==null?"":this.Model.GetValue("FTENDERTIME", i).ToString() },
+                            { "FCREATEDATE", this.Model.GetValue("FCREATEDATE", i)==null?"":this.Model.GetValue("FCREATEDATE", i).ToString() },
+                            { "FCONTRACTPARTY", this.Model.GetValue("FCONTRACTPARTY", i)==null?"":this.Model.GetValue("FCONTRACTPARTY", i).ToString() },
+                            { "FSeller", this.Model.GetValue("FSeller", i)==null?"":this.Model.GetValue("FSeller", i).ToString() },
+                            { "FPrjName", this.Model.GetValue("FPrjName", i)==null?"":this.Model.GetValue("FPrjName", i).ToString() },
+                            { "FMargin", this.Model.GetValue("FMargin", i)==null?"":this.Model.GetValue("FMargin", i).ToString() },
+                            { "FPubFundNo", this.Model.GetValue("FPubFundNo", i)==null?"":this.Model.GetValue("FPubFundNo", i).ToString() }
+                        };
+                    sendInfos.Add(info);
+                }
+            }
+
+            if (sendInfos.Count==0)
+            {
+                this.View.ShowErrMessage("请选择!");
+            }
+            else
+            {
+                String now = DateTime.Now.ToString("yyyy-MM-dd");
+                String appId = "PmtSummary";
+                String title = "title";
+                String PubAcctCode = "XT-4249be9f-5181-4a2a-a01f-7a9be93f1b3a";
+                String PubAcctKey = "4839223c64dff23b04bf4e3376b7a082";
+
+                List<Dictionary<string, string>> sendLogs = new List<Dictionary<string, string>>();
+
+                sendInfos.ForEach(info =>
+                {
+                    string text = string.Format(@"亲爱的中电家人[{0}],您在[{1}]所申请的[{2}]的[{3}]开标时间为[{4}]的投标保证金,金额为[{5}],该笔投标保证金预计退还日期为[{6}],请及时跟踪并反馈具体情况.谢谢!",
+                                            info["FSeller"], info["FCREATEDATE"], info["FCONTRACTPARTY"], info["FPrjName"],
+                                            info["FTENDERTIME"], info["FMargin"], info["FBackDate"]);
+
+                    Kingdee.BOS.Msg.XunTongMessage message = new Kingdee.BOS.Msg.XunTongMessage
+                    {
+                        AppId = appId,
+                        Title = title,
+                        Date = DateTime.Now.ToString("yyyy-MM-dd"),
+                        Text = text,
+                        PubAcctCode = PubAcctCode,//公共号帐号，移动BOS
+                        PubAcctKey = PubAcctKey,//公共号密钥
+                        Users = new List<string> { info["FOPENID"] }
+                    };
+
+                    XTSendResult res = XunTongServiceHelper.SendMessage(this.Context, message);
+
+                    Dictionary<string, string> log = new Dictionary<string, string>
+                    {
+                        { "order", info["FPubFundNo"] }
+                    };
+
+                    if (res.IsSuccess)
+                    {
+                        log.Add("res", "success");
+                    }
+                    else
+                    {
+                        log.Add("res", "error");
+                        log.Add("message", res.Msg);
+                    }
+
+                    sendLogs.Add(log);
+
+                });
+
+                string errorMessages = "";
+
+                sendLogs.Where(log => log["res"].Equals("error")).ToList().ForEach(log =>
+                {
+                    errorMessages +=  log["order"] + "发送失败:"+ log["message"]+"\n\r";
+                });
+
+                if (!errorMessages.Equals(""))
+                {
+                    this.View.ShowErrMessage(errorMessages.Substring(1));
+                }
+                else
+                {
+                    this.View.ShowMessage("消息已发送");
+                }
+            }
         }
 
         private void Act_Trans()
