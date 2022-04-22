0bcfabab9b6bc88766c2f756960b608a303e5d32
onechance <zhouh08@hotmail.com>
2022-04-22 09:56:42 +0800

    客户同步测试

---
+++ PlugIns/CRM/Sevice/CZ.CEEG.BosCrm.CustCopy/CZ_CEEG_BosCrm_CustCopy.cs
@@ -0,0 +1,65 @@
+﻿using CZ.Utils;
+using Kingdee.BOS.App.Data;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.BOS.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CZ.CEEG.BosCrm.CustCopy
+{
+    [Description("客户复制到其他系统")]
+    [HotUpdate]
+    public class CZ_CEEG_BosCrm_CustCopy : AbstractOperationServicePlugIn
+    {
+        public override void EndOperationTransaction(EndOperationTransactionArgs e)
+        {
+           
+        }
+
+        public override void AfterExecuteOperationTransaction(AfterExecuteOperationTransaction e)
+        {
+            CopyCus(e.DataEntitys[0]);
+        }
+
+        public string genInsertSql(DynamicObject obj,string TABLE_NAME) {
+
+            List<string> fieldNameList = new List<string>();
+            List<string> fieldValueList = new List<string>();
+
+            foreach (var propery in obj.DynamicObjectType.Properties)
+            {
+                fieldNameList.Add(propery.Name);
+                string value = obj[propery.Name].ToString();
+                if(value.Equals("0001-01-01 00:00:00")){
+                    value = "";
+                }
+                fieldValueList.Add("'"+value+"'");
+            }
+
+            return string.Format(
+                "INSERT INTO {0}({1}) VALUES({2});\n",
+                TABLE_NAME,
+                string.Join(", ", fieldNameList),
+                string.Join(", ", fieldValueList)
+            );
+        }
+
+
+        private void CopyCus(DynamicObject cus)
+        {
+            //根据客户id查数据库
+            String cusSql = "select * from T_BD_CUSTOMER where FCUSTID = " + cus["id"];
+
+            var objs = DBUtils.ExecuteDynamicObject(this.Context, cusSql);
+
+            if (objs.Count > 0) {
+                var cusDb = objs[0];
+                String sql = this.genInsertSql(cusDb, "T_BD_CUSTOMER2");
+                SqlUtil.ExecuteNonQuery(sql);
+            }
+        }
+    }
+}
---
+++ PlugIns/CRM/Sevice/CZ.CEEG.BosCrm.CustCopy/SqlUtil.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace CZ.Utils
+{
+    public class SqlUtil
+    {
+        private static readonly string connStr = "Data Source=10.4.200.187;Initial Catalog=AIS202104NTB;User ID=sa;Password=czkingdee";
+
+        /// <summary>
+        /// 执行增、删、改的方法
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pms"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(string sql, params SqlParameter[] pms)
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    con.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行返回DataTable的方法
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pms"></param>
+        /// <returns></returns>
+        public static DataTable ExecuteDataTable(string sql, params SqlParameter[] pms)
+        {
+            DataTable dt = new DataTable();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(sql, connStr))
+            {
+                if (pms != null)
+                {
+                    adapter.SelectCommand.Parameters.AddRange(pms);
+                }
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// 执行查询并返回一个实体的方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> ExecuteEntity<T>(string sql)
+        {
+            DataTable dt = ExecuteDataTable(sql, null);
+            Type type = typeof(T);
+            IEnumerable<PropertyInfo> properties = type.GetProperties();
+            List<T> entities = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T entity = Activator.CreateInstance<T>();
+                foreach (var p in properties)
+                {
+                    Type pType = p.PropertyType;
+                    if (pType == typeof(string))
+                    {
+                        p.SetValue(entity, row[p.Name].ToString(), null);
+                    }
+                    else if (pType == typeof(bool))
+                    {
+                        p.SetValue(entity, Convert.ToBoolean(row[p.Name]), null);
+                    }
+                    else if (pType == typeof(int))
+                    {
+                        p.SetValue(entity, Convert.ToInt32(row[p.Name]), null);
+                    }
+                    else if (pType == typeof(long))
+                    {
+                        p.SetValue(entity, Convert.ToInt64(row[p.Name]), null);
+                    }
+                    else if (pType == typeof(float))
+                    {
+                        p.SetValue(entity, float.Parse(row[p.Name].ToString()), null);
+                    }
+                    else if (pType == typeof(double))
+                    {
+                        p.SetValue(entity, Convert.ToDouble(row[p.Name]), null);
+                    }
+                    else if (pType == typeof(decimal))
+                    {
+                        p.SetValue(entity, Convert.ToDecimal(row[p.Name]), null);
+                    }
+                    else if (pType == typeof(DateTime))
+                    {
+                        p.SetValue(entity, Convert.ToDateTime(row[p.Name]), null);
+                    }
+                    // 其他类型不进行处理
+                }
+                entities.Add(entity);
+            }
+            return entities;
+        }
+    }
+}
